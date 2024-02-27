f716ac1e0b4c5a6c0528e877e024eed532738234
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-02-27 09:42:04 +0100

    unfinished Hotel

--- src/index.php
+++ src/index.php
@@ -2,3 +2,8 @@
 require_once 'vendor/autoload.php';
 
 
+$hotels = [
+    new Hotel('Luxor Oasis Resort & Casino', "Experience the grandeur of ancient Egypt amidst the modern luxury of the Las Vegas Strip at Luxor Oasis Resort & Casino. Indulge in lavish accommodations, world-class dining, and thrilling entertainment, all within the iconic pyramid structure that defines Las Vegas's skyline. Unveil the secrets of the pharaohs while enjoying unparalleled hospitality and excitement.", 4),
+    new Hotel('Mirage Heights Hotel & Spa', "Elevate your Las Vegas experience at Mirage Heights Hotel & Spa, where luxury meets serenity. Nestled along the vibrant Strip, our resort offers breathtaking views, exquisite dining options, and rejuvenating spa treatments. Unwind in style amidst lush surroundings and indulge in the ultimate Vegas getaway at Mirage Heights.", 4),
+    new Hotel('Stardust Skyline Resort & Casino', "Experience the grandeur of ancient Egypt amidst the modern luxury of the Las Vegas Strip at Luxor Oasis Resort & Casino. Indulge in lavish accommodations, world-class dining, and thrilling entertainment, all within the iconic pyramid structure that defines Las Vegas's skyline. Unveil the secrets of the pharaohs while enjoying unparalleled hospitality and excitement.", 4)
+];
