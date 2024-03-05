82b2003847528242aa468851c9f7eb4d8ecc1117
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-03-05 09:33:48 +0100

    changed to heredoc

--- public/hotels_with_loop.php
+++ public/hotels_with_loop.php
@@ -5,9 +5,15 @@ use OnPHPoint\WebtCoreViewsInMvc\Hotel;
 
 
 $hotels = [
-    new Hotel('Luxor Oasis Resort & Casino', "Experience the grandeur of ancient Egypt amidst the modern luxury of the Las Vegas Strip at Luxor Oasis Resort & Casino. Indulge in lavish accommodations, world-class dining, and thrilling entertainment, all within the iconic pyramid structure that defines Las Vegas's skyline. Unveil the secrets of the pharaohs while enjoying unparalleled hospitality and excitement.", 4),
-    new Hotel('Mirage Heights Hotel & Spa', "Elevate your Las Vegas experience at Mirage Heights Hotel & Spa, where luxury meets serenity. Nestled along the vibrant Strip, our resort offers breathtaking views, exquisite dining options, and rejuvenating spa treatments. Unwind in style amidst lush surroundings and indulge in the ultimate Vegas getaway at Mirage Heights.", 4),
-    new Hotel('Stardust Skyline Resort & Casino', "Experience the grandeur of ancient Egypt amidst the modern luxury of the Las Vegas Strip at Luxor Oasis Resort & Casino. Indulge in lavish accommodations, world-class dining, and thrilling entertainment, all within the iconic pyramid structure that defines Las Vegas's skyline. Unveil the secrets of the pharaohs while enjoying unparalleled hospitality and excitement.", 4)
+    new Hotel('Luxor Oasis Resort & Casino', <<<DESC
+    Experience the grandeur of ancient Egypt amidst the modern luxury of the Las Vegas Strip at Luxor Oasis Resort & Casino. Indulge in lavish accommodations, world-class dining, and thrilling entertainment, all within the iconic pyramid structure that defines Las Vegas's skyline. Unveil the secrets of the pharaohs while enjoying unparalleled hospitality and excitement.
+    DESC, 4),
+    new Hotel('Mirage Heights Hotel & Spa', <<<DESC
+    Elevate your Las Vegas experience at Mirage Heights Hotel & Spa, where luxury meets serenity. Nestled along the vibrant Strip, our resort offers breathtaking views, exquisite dining options, and rejuvenating spa treatments. Unwind in style amidst lush surroundings and indulge in the ultimate Vegas getaway at Mirage Heights.
+    DESC, 4),
+    new Hotel('Stardust Skyline Resort & Casino', <<<DESC
+    Experience the grandeur of ancient Egypt amidst the modern luxury of the Las Vegas Strip at Luxor Oasis Resort & Casino. Indulge in lavish accommodations, world-class dining, and thrilling entertainment, all within the iconic pyramid structure that defines Las Vegas's skyline. Unveil the secrets of the pharaohs while enjoying unparalleled hospitality and excitement.
+    DESC, 4)
 ];
 
 
@@ -16,8 +22,6 @@ $contents = fread($handle, filesize('../templates/template_loop.html'));
 
 $output = '';
 
-
-// parse contents of template.html using while loop
 
 $atCharacter = 0;
 
@@ -45,7 +49,6 @@ while ($atCharacter < strlen($contents)) {
             $inLoop = false;
             $atCharacter += 4;
         }
-        
     } elseif (substr($current, 0, 2) === '##') {
         $end = strpos($current, '##', 2);
         $key = substr($current, 2, $end - 2);
